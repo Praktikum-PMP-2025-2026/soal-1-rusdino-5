225fa022ddf3e1827027ca52d3600750433721a9
Rusydi Noor Abdurrahman <71594097+rusdino@users.noreply.github.com>
2026-05-06 08:27:31 +0700

    feat: Penambahan Solusi Awal

--- soal1.c
+++ soal1.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+typedef struct Pasien{
+    char id[100];
+    int time;
+}Pasien;
+
+void TampilkanWait(Pasien* x, int N){
+    int temp = 0;
+    for(int j =1; j< N; j++){
+        for(int i =0; i< j; i++){
+            temp += x[i].time;
+        }
+    }
+    printf("%d\n", temp);
+}
+
+void TampilkanUrut(Pasien* x, int N){
+    for(int i =0; i< N; i++){
+        printf("%s ", x[i].id);
+    }
+}
+
+int main(){
+    char buff[1024]; 
+    fgets(buff, sizeof(buff), stdin);
+    int N = atoi(strtok(buff, " "));
+    if(N!=0){
+    Pasien *x = (Pasien*)calloc(N, sizeof(Pasien));
+    for(int i =0; i < N; i++){
+        for(int j = 0; j <2; j++){
+        char *temp = strtok(NULL, " ");
+        if(temp != NULL){
+            if(j%2 == 0){
+                strcpy(x[i].id, temp);
+            }else if(j%2 == 1){
+                x[i].time = atoi(temp);
+            }
+            }
+        }
+
+    }
+    printf("ORDER: ");
+    TampilkanUrut(x, N);
+    printf("\nWAIT: ");
+    TampilkanWait(x, N);
+    
+}
 }
